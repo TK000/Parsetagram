52cee6df72903213594e41254c663d51d4028c3a
Tomoka Kan <tomokak@fb.com>
2018-07-10 14:20:01 -0700

    can take a picture now

--- app/src/main/java/com/tomoka/parsetagram/MainActivity.java
+++ app/src/main/java/com/tomoka/parsetagram/MainActivity.java
@@ -17,15 +17,24 @@ public class MainActivity extends AppCompatActivity {
     private EditText username_et;
     private EditText password_et;
     private Button login_btn;
+    private Button signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         username_et = findViewById(R.id.username_et);
         password_et = findViewById(R.id.password_et);
         login_btn = findViewById(R.id.login_btn);
+        signup_btn = findViewById(R.id.signup_btn);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void login(String username, String password) {
@@ -44,8 +62,9 @@ public class MainActivity extends AppCompatActivity {
             public void done(ParseUser user, ParseException e) {
                 if (e == null) {
                     Log.d("LoginActivity", "Login Successful");
+                    ParseUser currentUser = ParseUser.getCurrentUser();
 
-                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                     startActivity(intent);
                     finish();
                 } else {
---
+++ app/src/main/java/com/tomoka/parsetagram/SignupActivity.java
@@ -0,0 +1,62 @@
+package com.tomoka.parsetagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public EditText susername_et;
+    public EditText spassword_et;
+    public EditText semail_et;
+    public EditText shandle_et;
+    public Button submit_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        susername_et = findViewById(R.id.susername_et);
+        spassword_et = findViewById(R.id.spassword_et);
+        semail_et = findViewById(R.id.semail_et);
+        shandle_et = findViewById(R.id.shandle_et);
+        submit_btn = findViewById(R.id.submit_btn);
+    }
+
+    public void onSubmit(View v) {
+        final String username = susername_et.getText().toString();
+        final String password = spassword_et.getText().toString();
+        final String email = semail_et.getText().toString();
+        final String handle = shandle_et.getText().toString();
+
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.put("handle", handle);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("Signup", "Success");
+                    final Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.e("Signup", "failure");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
